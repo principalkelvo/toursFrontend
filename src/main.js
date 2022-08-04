1334074287d41478f33d9fe9f3c9060e52aee63d
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-04 03:57:21 +0300

    deployed url

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 // styles
-import '@/scss/main.scss'
+import "@/scss/main.scss";
 
 // core
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
 /* Vue. Main component */
-import App from './App.vue'
+import App from "./App.vue";
 
 /* Router & Store */
-import router from './router'
-import store from './store'
+import router from "./router";
+import store from "./store";
 
-import axios from 'axios'
+import axios from "axios";
 
-axios.defaults.baseURL= 'http://127.0.0.1:8000'
-createApp(App).use(store).use(router,axios).mount('#app')
+axios.defaults.baseURL = "https://traveltourskenya.herokuapp.com/";
+createApp(App).use(store).use(router, axios).mount("#app");
+// axios.defaults.baseURL= 'http://127.0.0.1:8000'
+// createApp(App).use(store).use(router,axios).mount('#app')
